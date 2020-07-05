d6a2435f747b2edd7f34b679af1fc3e6ae731fb9
shivang02 <shivang.dubey1@student.amity.edu>
2020-07-06 02:48:12 +0530

    Updates

--- precache-manifest.6a23c605db14a8e927c06327b3550f37.js
+++ precache-manifest.6a23c605db14a8e927c06327b3550f37.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b3e6e1e181e132cae7a85a922be163ce",
+    "revision": "bc0c1d62612b613f5fba20415db903f9",
     "url": "/markdown-previewer/index.html"
   },
   {
-    "revision": "a62377ac95af789e5222",
-    "url": "/markdown-previewer/static/css/main.4a3a6f1d.chunk.css"
+    "revision": "b4ac5853d1256fe96c0b",
+    "url": "/markdown-previewer/static/css/main.4639d4dd.chunk.css"
   },
   {
     "revision": "8eb067579698a91eaff3",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/markdown-previewer/static/js/2.52b2ad22.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a62377ac95af789e5222",
+    "revision": "b4ac5853d1256fe96c0b",
     "url": "/markdown-previewer/static/js/main.b86c7fe3.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/markdown-previewer/precache-manifest.d3af64181423447bec0e22cf8250f1d2.js"
+  "/markdown-previewer/precache-manifest.6a23c605db14a8e927c06327b3550f37.js"
 );
 
 self.addEventListener('message', (event) => {
